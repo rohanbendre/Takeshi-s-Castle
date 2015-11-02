4edaf904b3e431e762b3db5ae7da213c699b8eb3
Rohan Bendre <rohanbendre@Rohans-MacBook-Pro-2.local>
2015-11-02 01:40:57 -0500

    New level 1 design

---
+++ Assets/Scripts/LoadNewLevel.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadNewLevel : MonoBehaviour {
+		public void LoadLevel(){
+		Application.LoadLevel (2);
+	}
+
+}
---
+++ Assets/Scripts/TreasureBox.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TreasureBox : MonoBehaviour {
+
+	private MazeCell currentCell;
+	
+	void Update () {
+		
+	}
+
+	public void SetLocation (MazeCell cell) {
+		if (currentCell != null) {
+			currentCell.OnPlayerExited();
+		}
+		currentCell = cell;
+		//transform.localPosition = cell.transform.localPosition;
+		//currentCell.OnPlayerEntered();
+	}
+
+	 void OnCollisionEnter(Collision col) {
+		Debug.Log ("Key found!!");
+			if(col.gameObject.CompareTag("player"))
+		   {
+			Destroy(gameObject);
+			Application.LoadLevel(1);
+
+	}
+		}
+}
